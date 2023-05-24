4c877e403d9eef8506b9ed0c2b800bb1426c8a43
jcosses <120616033+jcosses@users.noreply.github.com>
2023-05-24 16:44:19 -0400

    Ingreso de Datos

---
+++ CLASE_12/datos.py
@@ -0,0 +1,9 @@
+# EJEMPLO DE USO DE GITHUB
+print("INGRESO DE DATOS")
+print("----------------\n")
+vNom = input("Ingrese su Nombre: ")
+vEdad = input("Ingrese su Edad: ")
+print("-------------------------")
+print(f"Su Nombres es {vNom}")
+print(f"Su Edad es {vEdad}")
+print("Prgama Finalizado..")
